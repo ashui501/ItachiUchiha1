4a72aa708bc341c90a6a96cfc80bc379ad9acc4f
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-23 13:05:30 +0530

    Update __main__.py

--- Itachi/__main__.py
+++ Itachi/__main__.py
@@ -60,7 +60,7 @@ async def main():
                              caption=strings.SUPPORT_SEND_MSG.format(platform.python_version(), pyrover, uptime)
                              )
         await ALPHA.send_message(config.OWNER_ID , "Started")
-        await ubot.send_message(f"{config.SUPPORT_CHAT}" , "Userbot Started ✨")
+        await ubot.send_message(f"{config.SUPPORT_CHAT}" , "**Userbot Started ✨**")
     except Exception as e:
         LOG.print(f"{e}")
         LOG.print(f"Bot isn't able to send message to @{config.SUPPORT_CHAT} !")
