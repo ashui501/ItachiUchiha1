58861f16e5019c4225d4e9837e56014017cd1665
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-24 18:39:21 +0530

    Add files via upload

--- Itachi/modules/pyro/permissions.py
+++ Itachi/modules/pyro/permissions.py
@@ -1,14 +1,18 @@
 from Itachi import app
 from Itachi.config import SUPER_USERS
-from pyrogram.enums import ChatMemberStatus
+from pyrogram.enums import ChatMemberStatus, ChatType
 
+async def is_group():
+    if message.chat.type == ChatType.PRIVATE:
+        return False
+    return True
 
-async def is_admin(chat,user):
-	user_id = (await app.get_users(user)).id
+async def is_admin(chat, user):
+    user_id = (await app.get_users(user)).id
     if user_id in SUPER_USERS:
-    	return True
+        return True
     else:
-    	chat_member = await app.get_chat_member(chat, user)
+        chat_member = await app.get_chat_member(chat, user)
         ctx = chat_member.privileges
         if ctx.can_delete_messages:
             return True
@@ -33,38 +37,38 @@ async def is_admin(chat,user):
         else:
             return False
 
-async def can_promote(chat,user):
-	check = (await app.get_chat_member(chat,user)).privileges.can_promote_members
-	if not check:
-		return False
+async def can_promote(chat, user):
+    check = (await app.get_chat_member(chat, user)).privileges.can_promote_members
+    if not check:
+        return False
     return True
 
-async def can_delete(chat,user):
-	check = (await app.get_chat_member(chat,user)).privileges.can_delete_messages
-	if not check:
-		return False
+async def can_delete(chat, user):
+    check = (await app.get_chat_member(chat, user)).privileges.can_delete_messages
+    if not check:
+        return False
     return True
 
-async def can_change_info(chat,user):
-	check = (await app.get_chat_member(chat,user)).privileges.can_change_info
-	if not check:
-		return False
+async def can_change_info(chat, user):
+    check = (await app.get_chat_member(chat, user)).privileges.can_change_info
+    if not check:
+        return False
     return True
 
-async def can_pin(chat,user):
-	check = (await app.get_chat_member(chat,user)).privileges.can_pin_messages
-	if not check:
-		return False
+async def can_pin(chat, user):
+    check = (await app.get_chat_member(chat, user)).privileges.can_pin_messages
+    if not check:
+        return False
     return True
 
-async def can_invite(chat,user):
-	check = (await app.get_chat_member(chat,user)).privileges.can_invite_users
-	if not check:
-		return False
+async def can_invite(chat, user):
+    check = (await app.get_chat_member(chat, user)).privileges.can_invite_users
+    if not check:
+        return False
     return True
 
-async def can_restrict(chat,user):
-	check = (await app.get_chat_member(chat,user)).privileges.can_restrict_members
-	if not check:
-		return False
+async def can_restrict(chat, user):
+    check = (await app.get_chat_member(chat, user)).privileges.can_restrict_members
+    if not check:
+        return False
     return True
