12736e04f2e4e4e567593fb934e1b2de914db43e
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-26 09:35:34 +0530

    Update chatGPT.py

--- Itachi/modules/chatGPT.py
+++ Itachi/modules/chatGPT.py
@@ -5,7 +5,7 @@ from pyrogram import *
 @app.on_message(filters.command("ask"))
 async def gpt(_,message):
     rep = await message.reply_text("💭")
-    if message.text < 1:
+    if len(message.text) < 1:
         return await rep.edit_text("**Give me a query too...**")
     text = message.text.split(maxsplit=1)[1]
     resp = requests.get("https://alphacoder-api.vercel.app/chatgpt" , params={"query":text})
