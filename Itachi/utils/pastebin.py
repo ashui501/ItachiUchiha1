922bef21b3faa614e15ae302e577e0c06f361582
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-26 16:06:17 +0530

    Add files via upload

---
+++ Itachi/utils/pastebin.py
@@ -0,0 +1,39 @@
+import socket
+from asyncio import get_running_loop
+from functools import partial
+from Itachi import aiohttpsession
+
+def _netcat(host, port, content):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    s.sendall(content.encode())
+    s.shutdown(socket.SHUT_WR)
+    while True:
+        data = s.recv(4096).decode("utf-8").strip("\n\x00")
+        if not data:
+            break
+        return data
+    s.close()
+
+async def paste(content):
+    loop = get_running_loop()
+    link = await loop.run_in_executor(
+        None, partial(_netcat, "ezup.dev", 9999, content)
+    )
+    return link
+
+async def post(url: str, *args, **kwargs):
+    async with aiohttpsession.post(url, *args, **kwargs) as resp:
+        try:
+            data = await resp.json()
+        except Exception:
+            data = await resp.text()
+    return data
+
+BASE = "https://batbin.me/"
+
+async def batbin_paste(content: str):
+    resp = await post(f"{BASE}api/v2/paste", data=content)
+    if not resp["success"]:
+        return
+    return BASE + resp["message"]
