36820bb662531cc53d73644515024bd0e8f71b01
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-26 10:45:35 +0530

    Update misc.py

--- Itachi/utils/misc.py
+++ Itachi/utils/misc.py
@@ -55,7 +55,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if max_num_pages != 0:
         modulo_page = page_n % max_num_pages
     else:
-        modulo_page = 0
+        modulo_page = 1
 
 
     # can only have a certain amount of buttons side by side
