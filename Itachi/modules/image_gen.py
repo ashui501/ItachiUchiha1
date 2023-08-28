b1c4914be0e2fa0eeeda4382ea6421d24755bbe1
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-28 15:04:11 +0530

    Update image_gen.py

--- Itachi/modules/image_gen.py
+++ Itachi/modules/image_gen.py
@@ -2,7 +2,6 @@ from pyrogram import Client, filters
 from Itachi import *
 import httpx
 
-url = "https://alphacoder-api-93747976af25.herokuapp.com"
 
 @Client.on_message(filters.command("draw"))
 async def gen_img(client, message):
@@ -11,8 +10,9 @@ async def gen_img(client, message):
     data = {"prompt": prompt}
     async with httpx.AsyncClient(timeout=30) as cli:
         try:
-            response = await cli.post(f"{url}/text2img", json=data)
-            pic = response.json().get("output_url")
+            response = await cli.post("https://alphacoder-api-93747976af25.herokuapp.com/text2img", json=data)
+            result = response.json()
+            pic = result.get("output_url")[0]
             await client.send_photo(message.chat.id, photo=pic, caption=f"**{message.from_user.mention} Here is your image.\nPrompt:** `{prompt}`")
             await temp.delete()
         except Exception as e:
