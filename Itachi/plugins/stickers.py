f0dffad1f7cefd9ed9e041c37c1b277eafb92b1c
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-29 20:43:33 +0530

    Add files via upload

---
+++ Itachi/plugins/stickers.py
@@ -0,0 +1,451 @@
+from pyrogram.enums import MessageMediaType
+import asyncio
+import os , httpx
+import re
+import shutil
+import cv2
+import ffmpeg,requests
+import tempfile,random
+from cloudscraper import CloudScraper
+from html import escape
+from PIL import Image
+from uuid import uuid4
+import pyrogram
+from pyrogram import emoji, filters, enums , Client
+from pyrogram.errors import BadRequest, PeerIdInvalid, StickersetInvalid
+from pyrogram.file_id import FileId
+from pyrogram.raw.functions.messages import GetStickerSet, SendMedia
+from pyrogram.raw.functions.stickers import (
+    AddStickerToSet,
+    CreateStickerSet,
+    RemoveStickerFromSet,
+)
+from pyrogram.raw.types import (
+    DocumentAttributeFilename,
+    InputDocument,
+    InputMediaUploadedDocument,
+    InputStickerSetItem,
+    InputStickerSetShortName,
+)
+from urllib.parse import quote as urlquote
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from bs4 import BeautifulSoup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+
+from Itachi import app as pgram ,BOT_USERNAME
+
+
+combot_stickers_url = "https://combot.org/telegram/stickers?q="
+LOG_CHANNEL = "MikuLog"
+http = httpx.AsyncClient(
+    http2=True,
+    timeout=httpx.Timeout(40),)
+
+
+@Client.on_message(filters.command("stickerid"))
+async def _stickid(_, message):
+    replied = message.reply_to_message
+    text = "**× Sticker ID :** `{}`"          
+         
+    if replied and replied.sticker:
+        await message.reply_text(text.format(replied.sticker.file_id))
+    else:
+        await message.reply_text("**Reply to a sticker in order to get ID**")
+        
+       
+
+@Client.on_message(filters.command("pkang"))
+async def _packkang(app,message):
+    txt = await message.reply_text("**Processing....**")
+    if not message.reply_to_message:
+        await txt.edit("**reply to a sticker**")
+        return
+    if not message.reply_to_message.sticker:
+        await txt.edit('**reply to a sticker**')
+        return
+    if message.reply_to_message.sticker.is_animated or  message.reply_to_message.sticker.is_video:
+        return await txt.edit("**reply to a non animated sticker**")
+    if len(message.command) < 2:
+        pack_name =  f'{message.from_user.first_name}_by_@{BOT_USERNAME}'
+    else :
+        pack_name = message.text.split(maxsplit=1)[1]
+    short_name = message.reply_to_message.sticker.set_name
+    stickers = await app.invoke(
+        pyrogram.raw.functions.messages.GetStickerSet(
+            stickerset=pyrogram.raw.types.InputStickerSetShortName(
+                short_name=short_name),
+            hash=0))
+    shits = stickers.documents
+    sticks = []
+    
+    for i in shits:
+        sex = pyrogram.raw.types.InputDocument(
+                id=i.id,
+                access_hash=i.access_hash,
+                file_reference=i.thumbs[0].bytes
+            )
+        
+        sticks.append(
+            pyrogram.raw.types.InputStickerSetItem(
+                document=sex,
+                emoji=i.attributes[1].alt
+            )
+        )
+
+    try:
+        short_name = f'stikcer_pack_{str(uuid4()).replace("-","")}_by_{app.me.username}'
+        user_id = await app.resolve_peer(message.from_user.id)
+        await app.invoke(
+            pyrogram.raw.functions.stickers.CreateStickerSet(
+                user_id=user_id,
+                title=pack_name,
+                short_name=short_name,
+                stickers=sticks,
+            )
+        )
+        await txt.edit(f"**Pack Kanged!**",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Your Pack",url=f"http://t.me/addstickers/{short_name}")]]))
+    except Exception as e:
+        await message.reply(str(e))
+
+
+
+@Client.on_message(filters.command("stickers"))
+async def _sticker(_, message):
+    if len(message.command) < 2 :
+        return await message.reply_text("**give me text to search stickers**")
+    split = message.text.split(" ", 1)
+    text = requests.get(combot_stickers_url + split[1]).text
+    soup = BeautifulSoup(text, "lxml")
+    results = soup.find_all("a", {"class": "sticker-pack__btn"})
+    titles = soup.find_all("div", "sticker-pack__title")
+    if not results:
+        await message.reply_text("**No results found.**")
+        return
+    reply = f"Stickers for *{split[1]}*:"
+    for result, title in zip(results, titles):
+        link = result["href"]
+        reply += f"\n• [{title.get_text()}]({link})"
+    await message.reply_text(reply, parse_mode=enums.ParseMode.MARKDOWN)
+
+
+@Client.on_message(filters.command("getvidsticker"))
+async def _vidstick(_, message):
+    chat_id = message.chat.id
+    replied = message.reply_to_message
+    if replied and replied.sticker:
+        if not replied.sticker.is_video:
+            return await message.reply_text("**Non Animated Stickers Not Allowed!**")
+        file_id = replied.sticker.file_id
+        new_file = await _.download_media(file_id, file_name="sticker.mp4")
+        await _.send_animation(chat_id,new_file)
+        os.remove(new_file)
+    else:
+        await message.reply_text("**reply to a video sticker**")
+
+@Client.on_message(filters.command("getvidgif"))
+async def _vidstick(_, message):
+    chat_id = message.chat.id
+    replied = message.reply_to_message
+    if replied and replied.animation:
+        
+        file_id = replied.animation.file_id
+        new_file = await _.download_media(file_id, file_name="video.mp4")
+        print(new_file)
+        await _.send_video(chat_id,video=open(new_file, "rb"))
+        os.remove(new_file)
+    else:
+        await message.reply_text("**reply to a gif")
+
+
+
+
+def get_emoji_regex():
+    e_list = [getattr(emoji, e).encode("unicode-escape").decode("ASCII") for e in dir(emoji) if not e.startswith("_")]
+    # to avoid re.error excluding char that start with '*'
+    e_sort = sorted([x for x in e_list if not x.startswith("*")], reverse=True)
+    # Sort emojis by length to make sure multi-character emojis are
+    # matched first
+    pattern_ = f"({'|'.join(e_sort)})"
+    return re.compile(pattern_)
+
+
+EMOJI_PATTERN = get_emoji_regex()
+SUPPORTED_TYPES = ["jpeg", "png", "webp"]
+
+
+
+
+@Client.on_message(filters.command("kang"))
+async def kang_sticker(c, m):
+    if m.sender_chat:
+        return await message.reply_text("**You are an anonymous admin.**")
+    prog_msg = await m.reply_text("**kanging sticker....**")
+    sticker_emoji = "🤔"
+    packnum = 0
+    packname_found = False
+    resize = False
+    animated = False
+    videos = False
+    convert = False
+    reply = m.reply_to_message
+    user = await c.resolve_peer(m.from_user.username or m.from_user.id)
+    if reply and reply.media:
+        if reply.photo:
+            resize = True
+        elif reply.animation:
+            videos = True
+            convert = True
+        elif reply.video:
+            convert = True
+            videos = True
+        elif reply.document:
+            if "image" in reply.document.mime_type:
+                # mime_type: image/webp
+                resize = True
+            elif reply.document.mime_type in (
+                enums.MessageMediaType.VIDEO,
+                enums.MessageMediaType.ANIMATION,
+            ):
+                # mime_type: application/video
+                videos = True
+                convert = True
+            elif "tgsticker" in reply.document.mime_type:
+                # mime_type: application/x-tgsticker
+                animated = True
+        elif reply.sticker:
+            if not reply.sticker.file_name:
+                return await prog_msg.edit_text("**The sticker has no name.**")
+            if reply.sticker.emoji:
+                sticker_emoji = reply.sticker.emoji
+            animated = reply.sticker.is_animated
+            videos = reply.sticker.is_video
+            if videos:
+                convert = False
+            elif not reply.sticker.file_name.endswith(".tgs"):
+                resize = True
+        else:
+            return await prog_msg.edit_text()
+
+        pack_prefix = "anim" if animated else "vid" if videos else "a"
+        packname = f"{pack_prefix}_{m.from_user.id}_by_{c.me.username}"
+
+        if len(m.command) > 1 and m.command[1].isdigit() and int(m.command[1]) > 0:
+            # provide pack number to kang in desired pack
+            packnum = m.command.pop(1)
+            packname = f"{pack_prefix}{packnum}_{m.from_user.id}_by_{c.me.username}"
+        if len(m.command) > 1:
+            # matches all valid emojis in input
+            sticker_emoji = "".join(set(EMOJI_PATTERN.findall("".join(m.command[1:])))) or sticker_emoji
+        filename = await c.download_media(m.reply_to_message)
+        if not filename:
+            # Failed to download
+            await prog_msg.delete()
+            return
+    elif m.entities and len(m.entities) > 1:
+        pack_prefix = "a"
+        filename = "sticker.png"
+        packname = f"c{m.from_user.id}_by_{c.me.username}"
+        img_url = next(
+            (m.text[y.offset : (y.offset + y.length)] for y in m.entities if y.type == "url"),
+            None,
+        )
+
+        if not img_url:
+            await prog_msg.delete()
+            return
+        try:
+            r = await http.get(img_url)
+            if r.status_code == 200:
+                with open(filename, mode="wb") as f:
+                    f.write(r.read())
+        except Exception as r_e:
+            return await prog_msg.edit_text(f"{r_e.__class__.__name__} : {r_e}")
+        if len(m.command) > 2:
+            # m.command[1] is image_url
+            if m.command[2].isdigit() and int(m.command[2]) > 0:
+                packnum = m.command.pop(2)
+                packname = f"a{packnum}_{m.from_user.id}_by_{c.me.username}"
+            if len(m.command) > 2:
+                sticker_emoji = "".join(set(EMOJI_PATTERN.findall("".join(m.command[2:])))) or sticker_emoji
+            resize = True
+    else:
+        return await prog_msg.edit_text("**Want me to guess the sticker?  Please tag a sticker.**")
+    try:
+        if resize:
+            filename = resize_image(filename)
+        elif convert:
+            filename = await convert_video(filename)  
+            if filename is False:
+                return await prog_msg.edit_text("Error")
+        max_stickers = 50 if animated else 120
+        while not packname_found:
+            try:
+                stickerset = await c.invoke(
+                    GetStickerSet(
+                        stickerset=InputStickerSetShortName(short_name=packname),
+                        hash=0,
+                    )
+                )
+                if stickerset.set.count >= max_stickers:
+                    packnum += 1
+                    packname = f"{pack_prefix}_{packnum}_{m.from_user.id}_by_{c.me.username}"
+                else:
+                    packname_found = True
+            except StickersetInvalid:
+                break
+        file = await c.save_file(filename)
+        media = await c.invoke(
+            SendMedia(
+                peer=(await c.resolve_peer(LOG_CHANNEL)),
+                media=InputMediaUploadedDocument(
+                    file=file,
+                    mime_type=c.guess_mime_type(filename),
+                    attributes=[DocumentAttributeFilename(file_name=filename)],
+                ),
+                message=f"#Sticker kang by UserID -> {m.from_user.id}",
+                random_id=c.rnd_id(),
+            ),
+        )
+        msg_ = media.updates[-1].message
+        stkr_file = msg_.media.document
+        if packname_found:
+            await prog_msg.edit_text("**Using existing sticker pack...**")
+            await c.invoke(
+                AddStickerToSet(
+                    stickerset=InputStickerSetShortName(short_name=packname),
+                    sticker=InputStickerSetItem(
+                        document=InputDocument(
+                            id=stkr_file.id,
+                            access_hash=stkr_file.access_hash,
+                            file_reference=stkr_file.file_reference,
+                        ),
+                        emoji=sticker_emoji,
+                    ),
+                )
+            )
+        else:
+            await prog_msg.edit_text("**Creating a new sticker pack...**")
+            stkr_title = f"{m.from_user.first_name}'s"
+            if animated:
+                stkr_title += "AnimPack"
+            elif videos:
+                stkr_title += "VidPack"
+            if packnum != 0:
+                stkr_title += f" v{packnum}"
+            try:
+                await c.invoke(
+                    CreateStickerSet(
+                        user_id=user,
+                        title=stkr_title,
+                        short_name=packname,
+                        stickers=[
+                            InputStickerSetItem(
+                                document=InputDocument(
+                                    id=stkr_file.id,
+                                    access_hash=stkr_file.access_hash,
+                                    file_reference=stkr_file.file_reference,
+                                ),
+                                emoji=sticker_emoji,
+                            )
+                        ],
+                        animated=animated,
+                        videos=videos,
+                    )
+                )
+            except PeerIdInvalid:
+                return await prog_msg.edit_text(
+                    "**It looks like you've never interacted with me in private chat, you need to do that first..**",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton(
+                                    "Click Me",
+                                    url=f"https://t.me/{c.me.username}?start",
+                                )
+                            ]
+                        ]
+                    ),
+                )
+
+    except BadRequest as e:
+        print(e)
+        return await prog_msg.edit_text("**Your Sticker Pack is full if your pack is not in v1 Type /kang 1, if it is not in v2 Type /kang 2 and so on.**")
+   # except Exception as all_e:
+   #     print(all_e)
+   #     await prog_msg.edit_text(f"{all_e.__class__.__name__} : {all_e}")
+    else:
+        markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="View Pack",
+                        url=f"https://t.me/addstickers/{packname}",
+                    )
+                ]
+            ]
+        )
+        await prog_msg.edit_text(
+            f"**Sticker Kanged!**",
+            reply_markup=markup,
+        )
+        # Cleanup
+        await c.delete_messages(chat_id=LOG_CHANNEL, message_ids=msg_.id, revoke=True)
+        try:
+            os.remove(filename)
+        except OSError:
+            pass
+
+
+def resize_image(filename: str) -> str:
+    im = Image.open(filename)
+    maxsize = 512
+    scale = maxsize / max(im.width, im.height)
+    sizenew = (int(im.width * scale), int(im.height * scale))
+    im = im.resize(sizenew, Image.NEAREST)
+    downpath, f_name = os.path.split(filename)
+    # not hardcoding png_image as "sticker.png"
+    png_image = os.path.join(downpath, f"{f_name.split('.', 1)[0]}.png")
+    im.save(png_image, "PNG")
+    if png_image != filename:
+        os.remove(filename)
+    return png_image
+
+
+
+async def convert_video(input):
+
+    vid = cv2.VideoCapture(input)
+    height = vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    width = vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+    # check height and width to scale
+    if width > height:
+        width = 512
+        height = -1
+    elif height > width:
+        height = 512
+        width = -1
+    elif width == height:
+        width = 512
+        height = 512
+
+    converted_name = "kangsticker.webm"
+
+    (
+        ffmpeg.input(input)
+        .filter("fps", fps=30, round="up")
+        .filter("scale", width=width, height=height)
+        .trim(start="00:00:00", end="00:00:03", duration="3")
+        .output(
+            converted_name,
+            vcodec="libvpx-vp9",
+            **{
+                #'vf': 'scale=512:-1',
+                "crf": "30"
+            },
+        )
+        .overwrite_output()
+        .run()
+    )
+
+    return converted_name
