8b5b8c8409175c5ce89c49d1912cd45d3536d2a2
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-31 08:12:04 +0530

    Create blacklist.py

---
+++ Itachi/modules/blacklist.py
@@ -0,0 +1,14 @@
+__help__ = """
+**Blacklisted words can't be used in group by normal users.**
+
+**Commands**
+
+♠ `/addblacklist <text>` - add words in blacklist.
+♠ `/rmblacklist - <text>` - remove blacklisted word.
+♠ `/unblacklistall` - unblacklist all blacklisted words ( owner only )
+♠ `/blacklists` - get all blacklisted words.
+♠ `/blacklistchat` - blacklist any chat. ( dev only )
+♠ `/blacklistchats` - get all blacklisted chats. ( dev only )
+
+"""
+__mod_name__ = "Blacklists 🔨"
