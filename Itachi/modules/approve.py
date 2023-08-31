d660cdfb0a23c4cf74d2e7547daf2df412258aad
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-31 07:38:20 +0530

    Create approve.py

---
+++ Itachi/modules/approve.py
@@ -0,0 +1,14 @@
+__help__ = """
+**Approval System For Group Safety.**
+
+**Commands**
+ 
+♠ `/approve <user>`: approved user antiflood won't be applied.
+♠ `/disapprove <user>`: disapprove user antiflood will be applied on them.
+♠ `/approved`: list of approved users.
+♠ `/approval <user>`: check the approval status of an user.
+♠ `/disapproveall`: disapprove all users. (owner only)
+
+"""
+
+__mod_name__ = "Approve 🙏"
