b9134c7c5bba507738301f8b28172e8562197075
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-31 06:51:33 +0530

    Create antiflood.py

---
+++ Itachi/modules/antiflood.py
@@ -0,0 +1,12 @@
+__help__ = """
+**AntiFlood Allows You To Take Action On Spammers.**
+
+**Commands**
+
+♠ `/flood` - get current flood settings.
+♠ `/setflood <on/off>` - set flood limit example - /serflood 10.
+♠ `/setfloodmode` - flood mode to ban , mute , kick user.
+
+"""
+__mod_name__ = "Anti-Flood"
+    
