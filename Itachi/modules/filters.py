3e3930ea6158bb51dd251d1bd8508f60ee5d661e
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-09-01 06:28:36 +0530

    Create filters.py

---
+++ Itachi/modules/filters.py
@@ -0,0 +1,12 @@
+__help__ = """
+**Filters to save in group**
+
+**Commands**
+
+♠   `/filter <name>` - add filter in a group.
+♠   `/stop <name>` - stop filter in a group.
+♠   `/removeallfilters` - owner only can do this.
+♠   `/filters` - check all filters in a group.
+
+"""
+__mod_name__ = "Filters 💥
