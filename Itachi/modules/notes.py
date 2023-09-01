d551c8374693b9e9694e77c5bc2b7a0b4cf3970a
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-09-01 06:33:19 +0530

    Create notes.py

---
+++ Itachi/modules/notes.py
@@ -0,0 +1,12 @@
+__help__ = """
+**Notes to save in group**
+
+**Commands**
+
+♠   `/save <name>` - save note in a group.
+♠   `/clear <name>` - clear note in a group.
+♠   `/clearall` - owner only can do this.
+♠   `/notes` - check all notes in a group.
+
+"""
+__mod_name__ = "Notes 🥀
