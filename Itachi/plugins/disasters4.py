c6cb5e435f7ba7a182979ea759a281acd1b4bd4f
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-09-02 11:02:42 +0530

    Create disasters4.py

---
+++ Itachi/plugins/disasters4.py
@@ -0,0 +1,19 @@
+from Itachi import app
+from Itachi.config import SUDO_USERS,DEV_USERS,SUPER_USERS as SUPREME_USERS , OWNER_ID
+from pyrogram import filters ,Client
+from Itachi.modules.pyro.extracting_id import extract_user_id
+from Itachi.modules.mongo.sudo_db import *
+from Itachi.modules.pyro.extracting_id import extract_user_id
+
+@Client.on_message(filters.command("akatsukis"))
+async def _devlist(_, message):
+    if message.from_user.id not in SUPREME_USERS:
+        return 
+    msg = "**♠ Akatsuki Users ♠\n**"
+    for m in set(DEV_USERS):
+        try:
+            mention = (await _.get_users(m)).mention
+            msg += f"• {mention}\n"
+        except Exception as e:
+            print(e)
+    return await message.reply_text(msg)
