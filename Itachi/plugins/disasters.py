3a59e5228b24a45fce6fafe90c6f7c65239427cc
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-09-02 10:59:47 +0530

    Update disasters.py

--- Itachi/plugins/disasters.py
+++ Itachi/plugins/disasters.py
@@ -22,18 +22,6 @@ async def _addsudo(_, message):
         SUPREME_USERS.append(user_id)
     return await message.reply_text("**Added In Kage Users.**")
 
-@Client.on_message(filters.command("rmkage") & filters.user(DEV_USERS))
-async def _rmsudo(_, message):
-    user_id = await extract_user_id(message)
-    if not user_id:
-        return await message.reply_text("**Specify An User.**")
-    if user_id not in SUDO_USERS:
-        return await message.reply_text("**This User Isn't Kage User.**")  
-    if user_id in SUDO_USERS:
-        SUDO_USERS.remove(user_id)
-        SUPREME_USERS.remove(user_id)
-    return await message.reply_text("**Removed From Kage Users.**")  
-
 
 
 @Client.on_message(filters.command("kages"))
