7d70d67d424df0f158ad2c5b9b83c91c079ee045
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-09-02 17:05:47 +0530

    Create locks.py

---
+++ Itachi/modules/locks.py
@@ -0,0 +1,12 @@
+__help__="""
+**Group Locks**
+
+**Commands**
+
+♠ `/locktypes` : get locktypes for chat.
+♠ `/lock <type>` : lock any perticular type.
+♠ `/unlock <type>` : unlock any perticular type.
+
+"""
+__mod_name__ = "Locks 🔐"
+
