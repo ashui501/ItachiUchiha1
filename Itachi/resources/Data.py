3d202ef16188758d8802310f781ab1909e142834
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-09-04 08:24:53 +0530

    Add files via upload

---
+++ Itachi/resources/Data.py
@@ -0,0 +1,31 @@
+LOCATION = [
+    "https://graph.org/file/9a150da51ac9e8c6f0a03.jpg",
+    "./TeleBot/resources/welcome_temp/minimal1.jpg",
+    "./TeleBot/resources/welcome_temp/minimal2.jpg",
+    "./TeleBot/resources/welcome_temp/AnimeMinimal1.jpg",
+    "./TeleBot/resources/welcome_temp/AnimeMinimal2.jpg",
+    "./TeleBot/resources/welcome_temp/AnimeMinimal3.jpg",
+    "./TeleBot/resources/welcome_temp/GamingMinimal.jpg",
+    "./TeleBot/resources/welcome_temp/Sun.jpg",
+    "./TeleBot/resources/welcome_temp/Gaming.jpg",
+    "./TeleBot/resources/welcome_temp/WS.jpg",
+    "./TeleBot/resources/welcome_temp/Nezuko.jpg",
+    "./TeleBot/resources/welcome_temp/AnimeGirl1.jpg",
+    "./TeleBot/resources/welcome_temp/AnimeGirl2.jpg"
+    ]
+
+WEL_PIC = [
+    "YEAH HERE I COULD USE DICT",
+    "https://graph.org/file/2fdd45220c2e1ff663728.jpg",
+    "https://graph.org/file/8fd55e8db5020c847a5ea.jpg",
+    "https://graph.org/file/349bdb8c2ed9e11933cdd.jpg",
+    "https://graph.org/file/05f38f34c2bba84ccf50c.jpg",
+    "https://te.legra.ph/file/7cf0e01f24c86b8ba5149.jpg",
+    "https://graph.org/file/4c4f243264938f16c6b2e.jpg",
+    "https://graph.org/file/6a726fa68699128e2a982.jpg",
+    "https://graph.org/file/2be097679acb0c3deaf72.jpg",
+    "https://te.legra.ph/file/7a34bdff1eae1e323287f.jpg",
+    "https://te.legra.ph/file/86ce7d280f899ddcf3b56.jpg",
+    "https://te.legra.ph/file/37b243d61a82508447bce.jpg",
+    "https://te.legra.ph/file/aba0071fb67e25040235d.jpg"
+]
