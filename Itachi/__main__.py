76fd9da2894158830fdad1eee120ebe0bcf4358e
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-09-04 10:55:47 +0530

    Update __main__.py

--- Itachi/__main__.py
+++ Itachi/__main__.py
@@ -40,7 +40,7 @@ from unidecode import unidecode
 from Itachi import StartTime , get_readable_time
 loop = asyncio.get_event_loop() 
 bot_name = f"{dispatcher.bot.first_name}"
-ITACHI_PIC = ["https://telegra.ph/file/f546e6681709b03255f00.jpg", "https://telegra.ph/file/fa1aab224fb2cbc08f47b.jpg", "https://telegra.ph/file/8b78e64eda3f0af73e186.jpg", "https://telegra.ph/file/1355fde2a2c876810ad02.jpg", "https://telegra.ph/file/607c2911f1dc48d40a4e9.jpg", "https://telegra.ph/file/14043c792ee8e71c4eeb6.jpg"]
+ITACHI_PIC = ["https://telegra.ph/file/986b76680ebc9d88f5c53.jpg", "https://telegra.ph/file/986b76680ebc9d88f5c53.jpg", "https://telegra.ph/file/986b76680ebc9d88f5c53.jpg", "https://telegra.ph/file/986b76680ebc9d88f5c53.jpg", "https://telegra.ph/file/986b76680ebc9d88f5c53.jpg", "https://telegra.ph/file/986b76680ebc9d88f5c53.jpg"]
 uptime = get_readable_time((time.time() - StartTime))
 WEBHOOK = False
 async def main():
