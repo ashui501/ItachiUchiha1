0c7d0658d054108d639263da5bdd31112be25e87
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-09-04 09:29:42 +0530

    Update chatGPT.py

--- Itachi/modules/chatGPT.py
+++ Itachi/modules/chatGPT.py
@@ -20,16 +20,18 @@ async def gpt(_,message):
     if len(message.text) < 1:
         return await rep.edit_text("**Give me a query too...**")
     text = message.text.split(maxsplit=1)[1]
-    resp = requests.get("https://alphacoder-api.vercel.app/llama" , params={"query":text})
+    url = "https://api.qewertyy.me/models"
+    params = {"model_id": 0, "prompt": text}
+    resp = requests.post(url , params=params)
     if resp.status_code == 200:
-        return await rep.edit_text(resp.json())
+        exe = resp.json()['content']
+        return await rep.edit_text(exe)
     else:
         return await rep.edit_text("**Currently API is Down!**")
 
 
 
-    
 
-    
 
-  
+
+
