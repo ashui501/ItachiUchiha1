30cac16212262ab8dfc01f40672a600773960b58
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-09-06 04:01:12 +0530

    Update goodbye.py

--- Itachi/plugins/goodbye.py
+++ Itachi/plugins/goodbye.py
@@ -1,26 +1,22 @@
-# Credits :- @TheStark
-
-from Itachi import app
-from pyrogram import filters , Client
+from pyrogram import filters, Client
 import uuid
 import math
 import os
 from PIL import Image, ImageDraw, ImageFont
 from unidecode import unidecode
 
-
-
+font_path = "./Itachi/resources/SuisseIntl-Regular.ttf"
+gif_path = "./Itachi/resources/sumi-sakurasawa-rent-a-girlfriend.gif"
 
 @Client.on_message(filters.left_chat_member)
-async def _left_mem(client,message):
+async def left_member_handler(client, message):
     first_name = unidecode(message.left_chat_member.first_name)
-    user_id = message.left_chat_member.id
  
-    goodbye = Image.open("./Itachi/resources/sumi-sakurasawa-rent-a-girlfriend.gif")
+    goodbye = Image.open(gif_path)
     stark = [goodbye.copy()]
 
     try:
-        while 1:
+        while True:
             goodbye.seek(goodbye.tell() + 4)
             owo = goodbye.copy()
             stark.append(owo)
@@ -34,14 +30,15 @@ async def _left_mem(client,message):
 
     s1 = stark[0].size[0] // 2
     s2 = 290
-    font = ImageFont.truetype("./Utachi/resources/SuisseIntl-Regular.ttf", 20)
+    font = ImageFont.truetype(font_path, 20)
     s3 = math.ceil(len(stark) / len(text))
 
     for i in range(len(stark)):
         draw = ImageDraw.Draw(stark[i])
         s4 = (s1 - len(text[i // s3]) * 5, s2)
         draw.text(s4, text[i // s3], font=font, anchor=None)
-    x =  f"welcome{uuid.uuid4()}.gif"
+    
+    x = f"goodbye_{uuid.uuid4()}.gif"
     stark[0].save(
         x,
         save_all=True,
@@ -50,5 +47,5 @@ async def _left_mem(client,message):
         duration=150,
         loop=0,
     )
-    await client.send_animation(message.chat.id,x,caption=f"**GoodBye {message.left_chat_member.mention}.Never come back again!**")
+    await client.send_animation(message.chat.id, x, caption=f"**Goodbye {message.left_chat_member.mention}. Never come back again!**")
     os.remove(x)
